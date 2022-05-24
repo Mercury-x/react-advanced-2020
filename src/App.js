f0303804786438fbbe694efb6596032a796cdd34
Mercury-x <1612395322@qq.com>
2022-05-24 13:29:25 +0800

    hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Setup from './tutorial/6-useReducer/setup';
+import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example';
 // import Setup from './tutorial/2-useEffect/final/3-useEffect-fetch-data';
 function App() {
   return (
