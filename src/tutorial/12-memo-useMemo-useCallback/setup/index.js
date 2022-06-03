8f5c99bf07cba36325cc3d17260ac631ba9e9710
Mercury-x <1612395322@qq.com>
2022-06-03 10:47:21 +0800

    react advanced complete

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,15 +1,40 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import { useFetch } from '../../9-custom-hooks/final/2-useFetch';
 
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/javascript-store-products'
+const url = 'https://course-api.com/javascript-store-products';
 
 // every time props or state changes, component re-renders
-
+const calculateMostExpensive = (data) => {
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price;
+      if (price > total) total = price;
+      return total;
+    }, 0) / 100
+  );
+};
 const Index = () => {
-  const { products } = useFetch(url)
-  const [count, setCount] = useState(0)
+  const { products } = useFetch(url);
+  const [count, setCount] = useState(0);
+  const [cart, setCart] = useState(0);
+  const mostExpensive = useMemo(
+    () => calculateMostExpensive(products),
+    [products]
+  );
+  console.log(mostExpensive);
+
+  const addCart = useCallback(() => {
+    setCart(cart + 1);
+  }, [cart]);
+
+  // when add cart change the state, Index will rerender
+  // and addCart will be a new func whitch has a new address
+  // so, the single item get a new prop, then all single item will rerender
+  // const addCart = () => {
+  //   setCart(cart + 1);
+  // };
 
   return (
     <>
@@ -17,32 +42,40 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1>Cart : {cart}</h1>
+      <h1>MostExpensiveProduct : ${mostExpensive}</h1>
+      <BigList products={products} addToCart={addCart} />
     </>
-  )
-}
+  );
+};
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}></SingleProduct>
+        );
       })}
     </section>
-  )
-}
+  );
+});
 
-const SingleProduct = ({ fields }) => {
-  let { name, price } = fields
-  price = price / 100
-  const image = fields.image[0].url
-
+const SingleProduct = ({ fields, addToCart }) => {
+  let { name, price } = fields;
+  price = price / 100;
+  const image = fields.image[0].url;
+  console.log('single item called');
   return (
     <article className='product'>
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
-  )
-}
-export default Index
+  );
+};
+export default Index;
